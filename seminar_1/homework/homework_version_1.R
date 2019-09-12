e235b8fa1a8c4815a9e0dbc193210e2ea3e94eaa
Marcian <marton.balazs.kovacs@gmail.com>
2019-09-12 09:36:01 +0200

    Extra hw update

--- seminar_1/homework/homework_version_1.R
+++ seminar_1/homework/homework_version_1.R
@@ -5,6 +5,7 @@
 ## A Drive mappa linkjét minden óra után a házifeladattal elküldjük nektek.
 
 # 1.feladat
+## Segítség: Nem kell egyesével beírni a számokat. Az órai anyagban megtaláljátok a megfejtést.
 ## Hozz létre egy objektumot "my_first_vector" néven, amiben benne vannak a számok egytől 120-ig.
 
 # 2.feladat
@@ -13,8 +14,8 @@
 # 3.feladat
 ## Ellenőrizd egy fügvénnyel, hogy tényleg 120 szám van-e benne.
 
-# 4.feladat
+# 4.feladat: EXTRA HÁZI
 ## Mentsd ki a "my_first_vector" első 20 elemét egy "my_second_vector" nevű obejktumba.
 
-# 5.feladat
+# 5.feladat: EXTRA HÁZI
 ## Töltsd le a "tidyverse" nevű packaget és töltsd be.
--- seminar_1/homework/homework_version_2.R
+++ seminar_1/homework/homework_version_2.R
@@ -5,6 +5,7 @@
 ## A Drive mappa linkjét minden óra után a házifeladattal elküldjük nektek.
 
 # 1.feladat
+## Segítség: Nem kell egyesével beírni a számokat. Az órai anyagban megtaláljátok a megfejtést.
 ## Hozz létre egy "numbers" nevű objektumot, amiben benne vannak a számok egytől százig.
 
 # 2.feladat
@@ -13,8 +14,8 @@
 # 3.feladat
 ## Ellenőrizd egy fügvénnyel, hogy tényleg 100 szám van-e benne.
 
-# 4.feladat
+# 4.feladat: EXTRA HÁZI
 ## Töröld ki a "numbers" változó első 50 elemét.
 
-# 5.feladat
+# 5.feladat: EXTRA HÁZI
 ## Töltsd le a "tidyverse" nevű packaget és töltsd be.
