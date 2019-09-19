ed76f9a57c68a394efdb51fa9bc3c71920e00582
Marcian <marton.balazs.kovacs@gmail.com>
2019-09-19 17:21:29 +0200

    Homework for class 2

---
+++ seminar_2/homework/homework_version_2.R
@@ -0,0 +1,34 @@
+# Házi feladat
+## Határidő: 2019.09.23. HÉTFŐ éjfél
+## Töltsd le ezt a kódot a saját számítógépedre
+## A kész házi feladatot mentsd el "vezeteknev_keresztnev_hazi_datum.R" néven a saját Drive mappádba.
+## A Drive mappa linkjét minden óra után a házifeladattal elküldjük nektek.
+
+# 1.feladat
+## A házifeladat elkészítéséhez telepítsd és töltsd be a "dplyr" és a "titanic" nevű csomagokat.
+
+# 2.feladat
+## Töltsd be a "titanic_train" nevű adattáblát és mentsd el egy "titanic_data" nevű objektumba.
+
+# 3.feladat
+## A Titanic korának lenagyobb hajója volt. De hányan utaztak rajta? Számold ki az órán tenült függvényekkel.
+
+# 4.feladat:
+## Hányan élték túl a katasztrófát és hányan vesztették az életüket?
+## Számold ki mindkét értéket és mentsd el egy "titanic_survived" nevű objektumba.
+## Segítség: Ha valaki túlélte a katasztrófát, az 1-es értéket kap a "survived" változóban, aki nem az 0-ás értéket.
+ 
+# 5.feladat:
+## Ahhoz, hogy érthetőbb legyen az előbb kiszámolt eredmény, kódold át a 0-ás értéket "elhunyt"-ra,
+## az 1-es értéket "tulelte"-re a "Survived" változóban a "titanic_survived" objektumban.
+
+# 6.feladat:
+## A "titanic_data" adattáblából szűrd ki csak az második osztályon utazókat.
+## Az, hogy ki melyik osztályon utazik a "Pclass" változóban van.
+
+# 7.feladat:
+## Rakd sorrendbe az utasokat asszerint, hogy ki mennyit fizetett a jegyért csökkenő sorrendben ("Fare").
+
+# 8.feladat:
+## A "Fare" változóban dollárban van a jegyek ára. Mennyibe kerülnének ezek a jegyek dollár centben?
+## Számold ki (1 dollár = 100 cent), és mentsd el egy új változóba "Fare_CENT".
