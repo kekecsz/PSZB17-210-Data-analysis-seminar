86b0be0dfa139bb07307baa4a603d0d68c1192ba
Marcian <marton.balazs.kovacs@gmail.com>
2019-09-26 11:07:22 +0200

    Added homework

---
+++ seminar_3/homework/homework_version_2.R
@@ -0,0 +1,31 @@
+# Házi feladat
+## Határidő: 2019.09.30. HÉTFŐ éjfél
+## Töltsd le ezt a kódot a saját számítógépedre
+## A kész házi feladatot mentsd el "vezeteknev_keresztnev_hazi_datum.R" néven a saját Drive mappádba.
+## A Drive mappa linkjét minden óra után a házifeladattal elküldjük nektek.
+
+# 1.feladat
+## A házifeladat elkészítéséhez telepítsd és töltsd be a "dplyr", a "ggplot2" és a "gapminder" nevű csomagokat.
+
+# 2.feladat
+## Töltsd be a "gapminder" nevű adattáblát és mentsd el egy "gapminder_data" nevű objektumba.
+
+# 3.feladat
+## Számold ki, hogy mekkora volt átlagos emberenkénti GDP (gdpPercap) kontinensenként (continent) 2007-ben.
+
+# 4.feladat:
+## Csinálj egy adattáblát, ami csak az 1992-ből származó adatokat tartalmazza
+## és mentsd el egy "gapminder_data_1992" nevű objektumba.
+ 
+# 5.feladat:
+## Ábrázold az országokban várható élettartamot a "gapminder_data_1992" adattáblát használva egy pont ábrán (scatterplot).
+## Az egyes megfigyelések (pontok) a kontinensek szerint legyenek beszínezve.
+
+# 6.feladat:
+## Nézd meg egy vonal ábrán hogyan alakult a Afrikában a várható élettartam a mérési évek során.
+
+# 7.feladat:
+## Nézd meg egy boxplotot használva, hogy milyen a várható életkorok eloszlása kontinensenként.
+
+# 8.feladat:
+## Ábrázold az emberenkénti GDP-t (gdpPercap) kontinensenként egy sűrűségi ábrán (density plot).
