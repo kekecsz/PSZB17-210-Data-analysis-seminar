15817a4616ad269ee8cc9be98a152a9efebbca2a
kekecsz <kekecs.zoltan@gmail.com>
2020-02-13 15:54:01 +0100

    Update homework_version_1.R

--- seminar_1/homework/homework_version_1.R
+++ seminar_1/homework/homework_version_1.R
@@ -1,15 +1,15 @@
 # Házi feladat
-## Határidő: 2019.09.15. éjfél
+
 ## Töltsd le ezt a kódot a saját számítógépedre
-## A kész házi feladatot mentsd el "vezeteknev_keresztnev_hazi_datum" néven a saját Drive mappádba.
-## A Drive mappa linkjét minden óra után a házifeladattal elküldjük nektek.
+## A kész házi feladatot mentsd el a saját Drive mappádba. A fájl neve ez legyen: 
+## Családnév Utónév házi feladat ÉÉÉÉHHNN, pl.: “Példa Géza házi feladat 20200212.R” ! 
 
 # 1.feladat
-## Segítség: Nem kell egyesével beírni a számokat. Az órai anyagban megtaláljátok a megfejtést.
 ## Hozz létre egy objektumot "my_first_vector" néven, amiben benne vannak a számok egytől 120-ig.
+## (Segítség: Nem kell egyesével beírni a számokat. Az órai anyagban megtaláljátok a megfejtést.)
 
 # 2.feladat
-## Milyen típusú változó a "my_first_vector" nevű objektum?
+## Milyen típusú adat a "my_first_vector" nevű objektum?
 
 # 3.feladat
 ## Ellenőrizd egy fügvénnyel, hogy tényleg 120 szám van-e benne.
