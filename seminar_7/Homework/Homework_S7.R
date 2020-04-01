a3752e7c08706ef2baf6c0e6baba5502eb1ba81c
kekecsz <kekecs.zoltan@gmail.com>
2020-04-01 19:53:01 +0200

    Add files via upload

--- seminar_7/Homework/Homework_S7.R
+++ seminar_7/Homework/Homework_S7.R
@@ -4,9 +4,9 @@
 # 2. Alakitsd at az adatokat ugy hogy dollar helyett forintban legyen a price valtozo, es negyzetlab helyett 
 # negyzetmeterben szerepljenek az adatok (ez a kovetkezo valtozokat erinti: 
 # sqft_living, sqft_lot, sqft_above, sqft_basement, sqft_living15, sqft_lot15) (1 negyzetlab = 0.9 m^2)
-# 3. futtass le egy korrelacios tesztet hogy meghatarozd, van-e egyuttjaras a lakas ara es a lakas allapota (condition) kozott (cor.test())
-# 4. Illessz egy egyszeru regresszios modellt amelyben a lakas arat (price) josolod be a lakas allapotaval (condition)
-# 5. Hasonlitsd ossze a teljes model F-teszt p-erteket, a condition regresszios egyutthatojahoz tartozo p-erteket, es a korrelacios teszt p-erteket. Mit veszel eszre?
+# 3. futtass le egy korrelacios tesztet hogy meghatarozd, van-e egyuttjaras a lakas ara es a lakas minosege (grade) kozott (cor.test())
+# 4. Illessz egy egyszeru regresszios modellt amelyben a lakas arat (price) josolod be a lakas minosegevel (grade) 
+# 5. Hasonlitsd ossze a teljes model F-teszt p-erteket, a grade regresszios egyutthatojahoz tartozo p-erteket, es a korrelacios teszt p-erteket. Mit veszel eszre?
 # 6. Hasonlitsd ossze az R^2 mutatot es a korrelacios egyutthato negyzetet. Mit veszel eszre?
 # 7. Most javitsd ezt a modelt ujabb prediktorok hozzaadasaval (hasznalj legalabb 3 prediktort a modellben).
 # 8. Mekkora a modell altal megmagyarazott varianciaarany?
