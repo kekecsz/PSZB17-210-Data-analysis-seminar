e51b86f6d68a90f3778721b787ba54564e5bd3af
kekecsz <kekecs.zoltan@gmail.com>
2020-09-06 13:08:38 +0200

    Add files via upload

---
+++ seminar_1/homework/S01_gyakorlas.R
@@ -0,0 +1,18 @@
+######################################
+# Az 1. gyakorlat gyakorlo script-je #
+######################################
+
+## Töltsd le ezt a kódot.
+## Oldd meg a feladatokat úgy hogy a kódot amit a megoldáshoz használsz az adott
+## feladat kommentje alá másolod.
+## A kész gyakorló feladatot mentsd el a saját Drive mappádba. 
+## A fájl neve ez legyen: 
+## Családnév Utónév házi feladat ÉÉÉÉHHNN, pl.: “Példa Géza házi feladat 20200212.R” ! 
+
+
+# 1. Rendeld hozzá a 4-et egy új number nevű objektumhoz.
+
+# 2. vond ki a number1 és a number2 összegéből ezt az objektumot, és az 
+# eredményt rendeld hozzá egy result nevű objektumhoz.
+
+# 3. listázd ki a result objektumot hogy leellenőrizd, helyes eredményt kaptál-e
