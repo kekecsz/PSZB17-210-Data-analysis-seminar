be9ce64523db0c8d87b72d24a2d659bbf3d66802
kekecsz <kekecs.zoltan@gmail.com>
2024-10-03 08:54:51 +0200

    Add files via upload

--- seminar_04/Homework/gyakorlas_S04.R
+++ seminar_04/Homework/gyakorlas_S04.R
@@ -7,40 +7,41 @@
 ## feladat kommentje alá másolod.
 ## A kész gyakorlófeladatot mentsd el a saját Drive mappádba. 
 ## A fájl neve ez legyen: 
-## Családnév Utónév gyakorlófeladat ÉÉÉÉHHNN, pl.: “Példa Géza gyakorlófeladat 20200912.R” !
+## Családnév Utónév gyakorlófeladat ÉÉÉÉHHNN, pl.: “Példa Géza gyakorlófeladat 20210928.R” !
 
 # 1. Toltsd be a szukseges package-eket. (Az alabbi feladatok megoldhatok a tidyverse es a psych package-ekkel)
 
 # 2. Olvasd be az adatokat errol az URL-rol (ez egy .csv file): 
-# https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv
-# Ha a fenti link nem mukodne, alternativ link az adattabla egy reszehez: "https://raw.githubusercontent.com/kekecsz/PSZB17-210-Data-analysis-seminar/master/seminar_4/COVID19_OWID_20200929.csv"
+# https://raw.githubusercontent.com/kekecsz/PSZB17-210-Data-analysis-seminar/refs/heads/master/seminar_04/StudentPerformanceFactors.csv
 
-# 3. Szurd az adatokat ugy, hogy csak a legutobbi datummal 
-# felvett adatok legyenek benne. Rendeld hozza ezt a szurt adatottablat egy uj objektumhoz
-# aminek a neve: COVID_latest
+# 3. Mi volt a legalacsonyabb részvételi arány (*Attendance*)?	
 
-# 4. Szamold ki hany regisztralt eset volt osszesen Magyarorszagon a legutolso datumig (*total_cases*)?	
+# 4. Mi az átlagos alvásmennyiség (*Sleep_Hours*) azok között a hallgatók között, akiknek alacsony a hozzéférése az erőforrásokhoz (Access_to_Resources == "Low")?	
 
-# 5. Mi volt a legmagasabb uj eset-szam Magyarorszagon eddig (*new_cases*)?
+# 5. Szurd az adatokat ugy hogy ne legyenek benne az otthon tanulo (School_Type == "Home") hallgatok.	
 
-# 6. Futtass le olyan leiro statisztikakat megado funkciot amibol latszik az egy millio 
-# fore eso uj esetek (*new_cases_per_million*) ferdesegi mutatoja (skew/skewness), es az is,
-# hany valid (nem NA, nem hianyzo adat) megfigyeles szerepel az adatbazisban az egy fore eso gdp-rol?
+# 6. Csinalj egy uj kategorikus valtozot (nevezzuk ezt *Sleep_Categorical*-nak) a mutate() funkcio hasznalataval amiben azok a diakok akiknek a  *Sleep_Hours* valtozo 6 alatti ertket mutat, "inadequate", ahol 6 vagy a felett van "adequate" kategoriaba keruljenek.	
+# Figyelj oda hogy faktorkent jelold meg ezt az uj valtozot (Ezt lehet az elozo lepesben a mutate() funkcion belul, vagy egy kulon lepesben, de mindenkeppen a factor() vagy az as.factor() funkciokat erdemes hozza hasznalni)	
+# 8. Mentsd el ezt a valtozot az eredeti adatobjektumban ugy hogy kesobb is lehessen vele dolgozni 	
+# 9. keszits egy tablazatot arrol, hogy hanyan esnek a *Sleep_Categorical* egyes kategoriaiba.	
+# 10. Add meg a faktorszintek helyes sorrendjet: az "inadequate" szint legyen elorebb sorolva, mint az "adequate" szint (Ird felul a *Sleep_Categorical* korabbi valtozatat ezzel a valtozattal ahol a szintek mar helyes sorrendben vannak, vagy ezt a sorrendezest is bele vonhatod az eredeti funkcioba, amivel a valtozot generaltad)	
+# 11. Ellenorizd, hogy valoban helyes sorrendben szerepelnek-e a faktor szintjei.	
 
-# 7. csinalj egy uj kategorikus valtozot (nevezzuk ezt *new_cases_per_million_kat*-nak)
-# a mutate() funkcio hasznalataval, amiben azok az orszagok ahol a  *new_cases_per_million*
-# valtozo 20 alatt van "small", ahol 20 vagy a felett van "large" kategoriaba keruljenek.	
+# 12. Szurd az adatokat ugy hogy csak a Previous_Scores, Peer_Influence, Parental_Involvement, es a Sleep_Hours valtozokkal dolgozz.	
+# - Hasznald a fent tanult modszereket, hogy **azonositsd a my_data adattablaban levo hibakat** vagy nem vart furcsasagokat.	
+# - A vizualizacion tul a View(), describe(), table(), es summary() funciokat erdemes hasznalni az adatok elso attekintesere 	
+# - A numerikus (vagy eppen folytonos) valtozoknal vizsgald meg a minimum es maximum erteket es a hianyzo adatok mennyiseget, valamint az eloszlast, esetleg a felvett ertekek mennyiseget, ha nincs tul sok felveheto ertek.	
+# - A kategorikus valtozoknal vizsgald meg az osszes faktorszintet es az egyes szintekhez tartozo megfigyelesek mennyiseget.	
 
-# 8. figyelj oda hogy faktorkent jelold meg ezt az uj valtozot 
+# 13. Hasznald a fent tanult modszereket, hogy megvizsgald a my_data adatbazisban a **Sleep_Categorical** es a **Distance_from_Home** valtozok kozotti osszefuggest.	
+# - hasznalj **geom_bar()** geomot a megjeleniteshez	
+# - probald meg mind a **szamossagot**, mind a **reszaranyt** kifejezo abrat megvizsgalni geom_bar(position = "fill")	
+# - milyen **kovetkeztetest** tudsz levonni az abrakrol?	
 
-# 9. mentsd el ezt a valtozot az eredeti adatobjektumban ugy hogy kesobb is lehessen vele dolgozni 	
+# 14. Hasznald a fent tanult modszereket, hogy megvizsgald az **Exam_Score** es a **Learning_Disabilities** valtozok kozotti osszefuggest.	
+# - keszits legalabb ket kulonbozo abrat mas-mas geomokkal	
 
-# 10. keszits egy tablazatot arrol, hogy hany megfigyeles van a *new_cases_per_million_kat* egyes kategoriaiban.	
+# 15. Milyen eros a kapcsolat az Exam_Score es a Sleep_Hours kozott?	
+# - hatarozd meg a korrelacios egyutthatot a valtozok kozott	
+# - abrazold a valtozok kapcsolatat	
 
-# 11. Add meg a faktorszintek helyes sorrendjet: small, large (Ird felul a *new_cases_per_million_kat*
-# korabbi valtozatat ezzel a valtozattal ahol a szintek mar helyes sorrendben vannak, 
-# vagy ezt a sorrendezest is bele vonhatod az eredeti funkcioba, amivel a valtozot generaltad)	
-
-# 12. Ellenorizd, hogy valoban helyes sorrendben szerepelnek-e a faktor szintjei.
-
-# 
